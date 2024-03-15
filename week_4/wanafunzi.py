41634dbe1dbf2a94f18324b944bb4d5a82b5c8e5
WAGITHI <159814975+WAGITHI@users.noreply.github.com>
2024-03-15 08:38:38 +0300

    Add files via upload

---
+++ week_4/wanafunzi.py
@@ -0,0 +1,18 @@
+#programme to  create a form
+#03/03/2024
+#Wagithi
+
+def print_name(name):
+    input(name)
+    print("My name is: {0}".format(name))
+
+def print_age(age):
+    input(age)
+    print("My age is: {0}".format(age))
+
+def print_yob(YOB):
+    input(YOB)
+    print("My year of birth is: {0}".format(YOB))
+
+    
+    
